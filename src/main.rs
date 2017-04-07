323b15853acc63bacfbf8adc6c714c1a382aa5ec
Nao Yonashiro <owan.orisano@gmail.com>
2017-04-07 20:28:58 +0900

    implement command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello,world!");
+    let matches = App::new("dialyze-rs")
+        .version("0.1")
+        .author("Nao YONASHIRO <owan.orisano@gmail.com>")
+        .about("dialyzer implemented by rust")
+        .arg(Arg::with_name("add_to_plt")
+             .long("add_to_plt"))
+        .arg(Arg::with_name("apps")
+             .long("apps")
+             .value_name("lib_dir")
+             .takes_value(true))
+        .arg(Arg::with_name("build_plt")
+             .long("build_plt"))
+        .arg(Arg::with_name("check_plt")
+             .long("check_plt"))
+        .arg(Arg::with_name("no_check_plt")
+             .short("n")
+             .long("no_check_plt"))
+        .arg(Arg::with_name("no_native")
+             .long("no_native"))
+        .arg(Arg::with_name("no_native_cache")
+             .long("no_native_cache"))
+        .arg(Arg::with_name("plt_info")
+             .long("plt_info"))
+        .arg(Arg::with_name("get_warnings")
+             .long("get_warnings"))
+        .get_matches();
+    let add_to_plt = matches.is_present("add_to_plt");
+    println!("{}", add_to_plt);
 }
